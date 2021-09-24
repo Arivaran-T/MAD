d012ecc3ab7f3792fcd14aecbc227c7853a6f7f7
IT20202422 <it20202422@my.sliit.lk>
2021-09-24 14:18:45 +0530

    User Home Page _ List View Creation(VerticalListView,HorizontalListView)

--- app/src/main/java/com/example/hairdo/UserHome.java
+++ app/src/main/java/com/example/hairdo/UserHome.java
@@ -5,77 +5,132 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
+import com.example.hairdo.FirebaseDBHandler.FirebaseDBHandler;
+import com.example.hairdo.Home.UserHomeSaloonHorizontalViewAdapter;
+import com.example.hairdo.Home.UserHomeSaloonVerticalListViewAdapter;
 import com.example.hairdo.model.Salon;
-import com.example.hairdo.model.Service;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
 
 public class UserHome extends Fragment {
-
-    //        String id = auth.getCurrentUser().getUid();
-    String id = "ejHLtEYSByaRAt0p7zp5yMaD9Na2";
-    ArrayList<Salon> myListData = new ArrayList<Salon>();
-    UserViewSalonList adapter ;
+    private DatabaseReference mDatabase;
+    private DatabaseReference childReference;
+    FirebaseDBHandler firebaseDBHandler;
+    private List<Salon> saloonList = new ArrayList<>();
+    private UserHomeSaloonHorizontalViewAdapter mAdapter;
+    private UserHomeSaloonVerticalListViewAdapter mAdapter2;
+    View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
+        /*firebaseDBHandler = new FirebaseDBHandler();
+        firebaseDBHandler.getAllSaloon();*/
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        childReference = mDatabase.child("Salon");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_user_home, container, false);
 
-        //set adapter
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.salons);
-        adapter = new UserViewSalonList(myListData,view.getContext());
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
-        recyclerView.setAdapter(adapter);
+        view = inflater.inflate(R.layout.fragment_user_home, container, false);
 
-        //fetch salons
-        Query query = FirebaseDatabase.getInstance().getReference("Salon");
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        mAdapter = new UserHomeSaloonHorizontalViewAdapter(saloonList);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        RecyclerView recyclerView2 = view.findViewById(R.id.recyclerView2);
+        mAdapter2 = new UserHomeSaloonVerticalListViewAdapter(saloonList);
+        LinearLayoutManager mLayoutManager2 = new LinearLayoutManager(getContext());
+        mLayoutManager2.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView2.setLayoutManager(mLayoutManager2);
+        recyclerView2.setItemAnimator(new DefaultItemAnimator());
+        recyclerView2.setAdapter(mAdapter2);
+
+
+        getAllSaloon();
+
+
+        return view;
+    }
+
+    public void getAllSaloon() {
+        ValueEventListener postListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                        Salon ser = dataSnapshot.getValue(Salon.class);
-                        ser.set_id(dataSnapshot.getKey());
-                        myListData.add(ser);
+                Log.d("DATA",snapshot.getValue().toString());
+//                Map<String, Object> childUpdates = new HashMap<>();
+
+                Log.d("saloon",snapshot.getValue().toString());
+
+                HashMap<String, Object> dataMap = (HashMap<String, Object>) snapshot.getValue();
+
+                for (String key : dataMap.keySet()){
+
+                    Object data = dataMap.get(key);
+
+                    try{
+                        HashMap<String, Object> userData = (HashMap<String, Object>) data;
+
+                        Log.d("Saloon",userData.get("name").toString());
+                        Log.d("Saloon",userData.get("address").toString());
+                        Log.d("Saloon",userData.get("contact").toString());
+
+                        Salon salon = new Salon(userData.get("name").toString(), userData.get("email").toString(),
+                                userData.get("address").toString(), userData.get("contact").toString(), userData.get("password").toString());
+
+                        saloonList.add(salon);
+
+                        Log.d("salon obj",salon.name);
+
+                    }catch (ClassCastException cce){
+
                     }
-                    adapter.notifyDataSetChanged();
 
                 }
+
+                mAdapter.notifyDataSetChanged();
+                mAdapter2.notifyDataSetChanged();
+
+
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(view.getContext(), "no servies available", Toast.LENGTH_SHORT).show();
-            }
-        });
 
-        return  view;
+            }
+
+
+        };
+        childReference.addValueEventListener(postListener);
     }
 
 
--- app/src/main/java/com/example/hairdo/UserProfile.java
+++ app/src/main/java/com/example/hairdo/UserProfile.java
@@ -67,9 +67,9 @@ public class UserProfile  extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected (MenuItem item) {
         int id = item.getItemId() ;
-        if (id == R.id. action_settings ) {
+        /*if (id == R.id. action_settings ) {
             return true;
-        }
+        }*/
         return super .onOptionsItemSelected(item) ;
     }
     @SuppressWarnings ( "StatementWithEmptyBody" )
@@ -115,7 +115,10 @@ public class UserProfile  extends AppCompatActivity
 
             Intent intent = new Intent(this,ContactUs.class);
             startActivity(intent);
-        }
+        }//else if(id == R.id.nav_offers){
+    //        Intent intent = new Intent(this,offer.class);
+    //        startActivity(intent);
+    //    }
 
 //        search.setOnClickListener(new View.OnClickListener() {
 //            @Override
